2cc60bc388475016a4c99ee88b2eb43d3f5beabc
Kaluvai-RajKumar <rajkumarkaluai155@gmail.com>
2025-08-10 11:13:47 +0530

    modified

--- 28072025.py
+++ 28072025.py
@@ -1,63 +1,24 @@
-import random
-class bank:
-    H_Detailsss=[]
-    def details(self):
-        H_Details={}
-        H_Details['name']=input('enter the name:')
-        H_Details['phone']=input('enter the phone no.:')
-        H_Details['aadhar']=input('enter the aadhar:')
-        data=random.randint(1000000000,9999999999)
-        H_Details['Account no.']=data
-        p1=input('which acount is required savings/zero: ') 
-        while True:  
-            if p1=='savings':
-                p2=int(input('enter the account to deposite 1000 :'))
-                if p2==1000:
-                    H_Details['balance']=p2
-                    break
-                else:
-                    print('please deposite min. account')   
-            elif p1=='zero':
-                p3=int(input('enter the account to deposite 500 :'))
-                if p3==500:
-                    H_Details['balance']=p3
-                    break
-                else:
-                    print('please deposite min. account') 
-        bank.H_Detailsss.append(H_Details) 
-        print(bank.H_Detailsss) 
-    def deposite(self):
-            name1=input('enter name:')
-            account_number=int(input('enter account no.:'))
-            amount=int(input('enter the account to deposite: '))
-            for x in bank.H_Detailsss:
-                if x['name'] == name1 and x['Account no.'] == account_number:
-                    data2=x.get('balance')
-                    x['balance']=data2+amount
-                    break
-                else:
-                    print('incorrect details')   
-            print(bank.H_Detailsss)         
+from abc import ABC,abstractmethod
+class Bank(ABC):
+    @abstractmethod
+    def deposit(self):
+        pass
+    @abstractmethod
     def withdraw(self):
-            name1=input('enter name:')
-            account_number=int(input('enter account no.:'))
-            amount1=int(input('enter the account to withdraw: '))
-            for x in bank.H_Detailsss:
-                if x['name']==name1 and x['Account no.']==account_number:
-                    data3=x.get('balance')
-                    if amount1 <= data3:
-                        x['balance']-=amount1
-                        break
-                    else:
-                        print('insufficient balance')    
-                else:
-                    print('incorrect details')                        
-            print(bank.H_Detailsss)       
-obj=bank()
-obj.details()
-obj.deposite()
-obj.withdraw()
-
-
-
-
+        pass
+class union(Bank):
+    def __init__ (self):
+        self.balance=0
+    def deposit(self,amount):
+        self.balance += amount
+        print(self.balance)
+    def withdraw(self,amount):
+        if amount < self.balance:
+            self.balance -= amount
+            print(self.balance)
+        else:
+            print("Check Your Balnace")
+obj=union()
+obj.deposit(300000)
+obj.withdraw(198345)
+obj.withdraw(4000000)
